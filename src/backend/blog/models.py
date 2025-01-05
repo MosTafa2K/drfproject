d30589c1e004ae11669bc7a9a5ab765e8ecdd0fa
mostafa2k <m2kappswindows8.1@gmail.com>
2025-01-05 12:07:50 +0330

    refactor(blog): remove `date` field from `Post` model
    - Removed the unnecessary `date` field from the `Post` model in the `blog` app.
    - Updated the database schema to reflect this change

--- src/backend/blog/models.py
+++ src/backend/blog/models.py
@@ -7,7 +7,6 @@ class Article(models.Model):
     title = models.CharField(max_length=150)
     slug = models.SlugField()
     content = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(upload_to="images/article_covers", null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
