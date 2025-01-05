e06674229a1da82908378c8f454ee97af1685b60
mostafa2k <m2kappswindows8.1@gmail.com>
2025-01-05 21:17:57 +0330

    refactor(blog): update `slug` field in `Article` model
    - Modified the `slug` field in the `Article` model to accept null and blank values (`null=True`, `blank=True`).

--- src/backend/blog/models.py
+++ src/backend/blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=150)
-    slug = models.SlugField()
+    slug = models.SlugField(null=True, blank=True)
     content = models.TextField()
     thumb = models.ImageField(upload_to="images/article_covers", null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
