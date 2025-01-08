8173cbf87b469028e87690e7acef2d5ebe6d86c5
mostafa2k <m2kappswindows8.1@gmail.com>
2025-01-08 07:41:53 +0330

    feat(core): configure login redirect and include API URLs
    - Set `LOGIN_REDIRECT_URL` in settings to redirect users to `/api/users/me/` after login.
    - Add `api/` route to include API URLs in the main URL configuration.
    - Remove static and media URL configurations under DEBUG condition from `core/urls.py`.

--- src/backend/core/urls.py
+++ src/backend/core/urls.py
@@ -24,9 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("", include("blog.urls")),
+    path("api/", include("api.urls")),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
